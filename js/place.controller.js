0211296a10f825b3e6e7557492f8aa980efa72cb
Roi Doron <roidoron6@gmail.com>
2024-02-25 21:31:51 +0200

    Add basic controller and service map

--- js/place.controller.js
+++ js/place.controller.js
@@ -1,1 +1,35 @@
 'use strict'
+
+function onInit() {
+    renderSite()
+    renderPlaces()
+}
+
+function renderSite() {
+    const user = getUser()
+    const elBody = document.querySelector('body')
+    elBody.style.color = user.txtColor
+    elBody.style.backgroundColor = user.bgColor
+}
+
+function renderPlaces() {
+    const places = getPlaces()
+    console.log(places);
+
+
+    const strHTML = places.map(place =>
+        `<li>
+        <span class="name">${place.name}</span>
+        <button onclick="onRemovePlace('${place.id}')" class="delet">x</button>
+        <button class="go">Go</button>
+    </li>`
+    )
+    const elUl = document.querySelector('.places-list')
+
+    elUl.innerHTML = strHTML.join('')
+}
+
+function onRemovePlace(placeId) {
+    RemovePlace(placeId)
+    renderPlaces()
+}
--- js/service/place.service.js
+++ js/service/place.service.js
@@ -1,19 +1,46 @@
 'use strict'
- const places = []
+var gPlaces
+
+_createPlaces()
 
 
-function createPlaces(){
-createPlace(31.813603,34.772725,'Roi house')
-createPlace(32.061099,34.791867,'Menora')
+
+function getPlaces() {
+    
+    return gPlaces
+}
+
+function RemovePlace(placeId) {
+    const placesIdx = gPlaces.findIndex(place => place.id === placeId)
+    gPlaces.splice(placesIdx, 1)
+    // _saveBooks()
+}
+
+function getPlaceById(placeId) {
+    const place = gPlaces.find(place => place.il === placeId)
+    return place
 
 }
 
+function addPlace(name, lat, lng, zoom) {
+    const newPlace = _createPlace(lat, lng, name,zoom)
+    gPlaces.push(newPlace)
+    return newPlace
+}
 
- function createPlace(lat,lng,name){
+function _createPlaces() {
+    gPlaces = [
+        _createPlace(31.813603, 34.772725, 'Roi house'),
+        _createPlace(32.061099, 34.791867, 'Menora')
+    ]
+
+}
+
+function _createPlace(lat, lng, name,zoom) {
     return {
-        id: makeId,
+        id: makeId(),
         lat,
         lng,
         name
     }
- }
+}
